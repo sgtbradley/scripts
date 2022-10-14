22d019815c2d1dbf761a8a5f718fa067c1a6d638
Josh <35059872+jazzakek@users.noreply.github.com>
2022-10-14 13:49:31 +1100

    more debian

---
+++ debian/fish-setup.sh
@@ -0,0 +1,4 @@
+#!/bin/fish
+sudo apt install -y curl
+curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
+omf install bang-bang bobthefish
--- debian/init.sh
+++ debian/init.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 sudo apt update && sudo apt upgrade -y
 sudo apt install fish
-echo /usr/local/bin/fish | sudo tee -a /etc/shells
-chsh -s /usr/local/bin/fish
+chsh -s /bin/fish
 echo "Please restart your session"
