41e26ea326d03aa8b2206174e3801bb38fc273c1
Josh <35059872+jazzakek@users.noreply.github.com>
2022-10-14 14:19:07 +1100

    change theme to syl20bnr

--- debian/1-fish.sh
+++ debian/1-fish.sh
@@ -1,4 +1,4 @@
 #!/bin/fish
 sudo apt install -y curl
 curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
-omf install bang-bang bobthefish
+omf install bang-bang syl20bnr
